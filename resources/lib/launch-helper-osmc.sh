8fdbd67f8e4f709bcc4d219f29e5dbc34583cb1b
Ludwig Wacker <wacker@gardenofconcepts.com>
2015-12-13 13:43:01 +0100

    LUNA: heartbeat script and added credit where credit is due

--- resources/lib/launch-helper-osmc.sh
+++ resources/lib/launch-helper-osmc.sh
@@ -1,13 +1,20 @@
 #!/bin/sh
+# Originally written by miko
+# Modified by dodslaser
+# Modified again by wackerl91 (support for launch args)
 
 LAUNCHER_PATH=$1
-GAME=$2
-CONF_PATH=$3
+HEARTBEAT_PATH=$2
+GAME=$3
+CONF_PATH=$4
 
+sudo su osmc -c "sh $HEARTBEAT_PATH &" &
 sudo su osmc -c "nohup openvt -c 7 -s -f sh $LAUNCHER_PATH $GAME $CONF_PATH >/dev/null 2>&1 &" &
+
 sudo openvt -c 7 -s -f clear
 
 sleep 2
 
 sudo su -c "systemctl stop mediacenter &" &
+
 exit
---
+++ resources/lib/moonlight-heartbeat.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+sleep 10
+while [ true ]; do
+        status="$(pidof moonlight | wc -w)"
+        if [ ${status} -ne 1 ]; then
+            sudo su -c "sudo systemctl restart mediacenter &" &
+            exit
+        else
+            sleep 2
+        fi
+done

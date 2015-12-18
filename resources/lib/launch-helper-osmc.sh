967312eae4421d1d2a4aacf05bd08e099837df05
Ludwig Wacker <wacker@gardenofconcepts.com>
2015-12-18 15:06:04 +0100

    LUNA: proper passing of arguments in script; corrected file permissions

--- resources/lib/launch-helper-osmc.sh
+++ resources/lib/launch-helper-osmc.sh
@@ -9,7 +9,7 @@ GAME=$3
 CONF_PATH=$4
 
 sudo su osmc -c "sh $HEARTBEAT_PATH &" &
-sudo su osmc -c "nohup openvt -c 7 -s -f sh $LAUNCHER_PATH $GAME $CONF_PATH >/dev/null 2>&1 &" &
+sudo su osmc -c "nohup openvt -c 7 -s -f sh $LAUNCHER_PATH \"${GAME}\" $CONF_PATH >/dev/null 2>&1 &" &
 
 sudo openvt -c 7 -s -f clear
 

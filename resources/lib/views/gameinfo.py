1ee34a1e9c3bba8bd386179563ee5f113d655980
Ludwig Wacker <wacker@gardenofconcepts.com>
2015-12-30 22:31:45 +0100

    LUNA-13: Added 'choose cover art' button to game info view

--- resources/lib/views/gameinfo.py
+++ resources/lib/views/gameinfo.py
@@ -1,10 +1,16 @@
 import os
 
-import pyxbmct.addonwindow as pyxbmct
+import addonwindow as pyxbmct
 
 from xbmcswift2 import xbmc, xbmcaddon, xbmcgui
 
 _addon_path = xbmcaddon.Addon().getAddonInfo('path')
+
+COLOR_FO = '0xFFE0B074'
+COLOR_NF = '0xFF808080'
+COLOR_HEADING = '0xFFD6D6D6'
+COLOR_DETAILS = '0xFF707070'
+COLOR_SELECTED = '0xFFF1F1F1'
 
 
 class GameInfo(pyxbmct.AddonDialogWindow):
@@ -29,22 +35,30 @@ class GameInfo(pyxbmct.AddonDialogWindow):
         self.set_active_controls(game)
         self.set_navigation()
         self.connect(pyxbmct.ACTION_NAV_BACK, self.close)
+        # init controls
+        self.image = None
+        self.genre = None
+        self.year = None
+        self.plot = None
+        self.button_play = None
+        self.button_cover_art = None
+        self.button_fanart = None
 
     def set_info_controls(self, game):
-        title_label = pyxbmct.Label(game.name, alignment=pyxbmct.ALIGN_LEFT, font='font14')
+        title_label = pyxbmct.Label(game.name, alignment=pyxbmct.ALIGN_LEFT, font='XLarge', textColor=COLOR_HEADING)
         self.placeControl(title_label, 0, 0, 2, 3)
 
         self.image = pyxbmct.Image(game.poster)
         self.placeControl(self.image, 2, 0, 6, 1)
 
-        genre_label = pyxbmct.Label('Genre', alignment=pyxbmct.ALIGN_LEFT)
+        genre_label = pyxbmct.Label('Genre', alignment=pyxbmct.ALIGN_LEFT, font='Med', textColor=COLOR_DETAILS)
         self.placeControl(genre_label, 2, 2)
-        self.genre = pyxbmct.Label(game.get_genre_as_string(), alignment=pyxbmct.ALIGN_LEFT)
+        self.genre = pyxbmct.Label(game.get_genre_as_string(), alignment=pyxbmct.ALIGN_LEFT, font='Med')
         self.placeControl(self.genre, 2, 3, columnspan=3)
 
-        year_label = pyxbmct.Label('Year', alignment=pyxbmct.ALIGN_LEFT)
+        year_label = pyxbmct.Label('Year', alignment=pyxbmct.ALIGN_LEFT, font='Med', textColor=COLOR_DETAILS)
         self.placeControl(year_label, 3, 2)
-        self.year = pyxbmct.Label(game.year, alignment=pyxbmct.ALIGN_LEFT)
+        self.year = pyxbmct.Label(game.year, alignment=pyxbmct.ALIGN_LEFT, font='Med')
         self.placeControl(self.year, 3, 3)
 
         self.plot = pyxbmct.TextBox()
@@ -52,13 +66,19 @@ class GameInfo(pyxbmct.AddonDialogWindow):
         self.plot.setText(game.plot)
 
     def set_active_controls(self, game):
-        self.button_play = pyxbmct.Button('Play', focusTexture='', noFocusTexture='', focusedColor='0xFFE50000')
+        self.button_play = pyxbmct.Button('Play', focusTexture='', noFocusTexture='', focusedColor=COLOR_FO,
+                                          textColor=COLOR_NF, font='Med')
         self.placeControl(self.button_play, 11, 0)
         self.connect(self.button_play, self.launch_game)
 
+        self.button_cover_art = pyxbmct.Button('Choose Cover Art', focusTexture='', noFocusTexture='',
+                                               focusedColor=COLOR_FO, textColor=COLOR_NF, font='Med')
+        self.placeControl(self.button_cover_art, 11, 1)
+        self.connect(self.button_cover_art, self.select_cover_art)
+
         self.button_fanart = pyxbmct.Button('Choose Fanart', focusTexture='', noFocusTexture='',
-                                            focusedColor='0xFFE50000')
-        self.placeControl(self.button_fanart, 11, 1)
+                                            focusedColor=COLOR_FO, textColor=COLOR_NF, font='Med')
+        self.placeControl(self.button_fanart, 11, 2)
         self.connect(self.button_fanart, self.select_fanart)
 
     def set_navigation(self):
@@ -76,9 +96,17 @@ class GameInfo(pyxbmct.AddonDialogWindow):
 
     def select_fanart(self):
         browser = xbmcgui.Dialog().browse(2, 'Select Fanart', 'files', '.jpg|.png', False, False,
-                                          self.game.fanarts[0])
-        self.game.selected_fanart = browser
-        self.container.get_core().get_storage().sync()
+                                          self.game.get_fanart(0, ''))
+        if browser:
+            self.game.selected_fanart = browser
+            self.container.get_core().get_storage().sync()
+
+    def select_cover_art(self):
+        browser = xbmcgui.Dialog().browse(2, 'Select Cover Art', 'files', '.jpg|.png', False, False,
+                                          self.game.get_poster(0, ''))
+        if browser:
+            self.game.selected_poster = browser
+            self.container.get_core().get_storage().sync()
 
     def setAnimation(self, control):
         control.setAnimations(

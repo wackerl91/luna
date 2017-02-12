e8466cb7046fadbd2200e7d0bf470fd3b65bf281
Ludwig Wacker <wacker@gardenofconcepts.com>
2017-02-12 14:09:05 +0100

    LUNA-67: Closing all dialogs on exception to prevent users being stuck

--- resources/lib/kernel/__init__.py
+++ resources/lib/kernel/__init__.py
@@ -17,10 +17,12 @@ def log_exception(exctype, value, tb):
     import xbmcgui
     xbmcgui.Dialog().notification(
         'Luna',
-        'An exception occurred and has been reported to Eos. Please check the log for more information.'
+        'An exception occurred and has been reported to Eos. Please check the log for more information.',
+        time=10000
     )
 
     import xbmc
     xbmc.log("[script.luna]: %s - %s" % (str(exctype), str(value)), xbmc.LOGERROR)
+    xbmc.executebuiltin('Dialog.Close(all,true)')
 
 sys.excepthook = log_exception
